739c8ce2349f46d7cab4ff8573fe1ffb9e7067a3
yerim216 <bing_01@naver.com>
2024-03-26 21:15:30 +0900

    초기 환경 설정

---
+++ src/test/java/com/example/nzgeneration/NzGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nzgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NzGenerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
