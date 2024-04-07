eea8c2b15457fb3e91e1c41160f29f75ddb37683
yerim216 <bing_01@naver.com>
2024-04-07 21:49:46 +0900

    Feat: submodule 설정, 주석 제거

--- src/main/java/com/example/nzgeneration/TestController.java
+++ src/main/java/com/example/nzgeneration/TestController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/* TestController.java */
 @RestController
 public class TestController {
     @GetMapping("/hello")
