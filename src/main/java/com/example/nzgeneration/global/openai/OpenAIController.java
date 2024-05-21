a3d16d5b802f3fa6327e7e706279846784deef17
gourderased <117848386+gourderased@users.noreply.github.com>
2024-05-21 19:41:21 +0900

    Feat: #5 - nft이미지 생성 구현

--- src/main/java/com/example/nzgeneration/global/openai/OpenAIController.java
+++ src/main/java/com/example/nzgeneration/global/openai/OpenAIController.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +16,18 @@ public class OpenAIController {
 
     private final OpenAIService openAIService;
 
-    @GetMapping("api/openai/image-safety")
+    @GetMapping("test/openai/image-safety")
     public ResponseEntity<Boolean> checkImageSafety(@RequestParam String imageUrl)
         throws JsonProcessingException {
-            Boolean result = openAIService.getImageDescription(imageUrl);
-            return ResponseEntity.ok(result);
+        Boolean result = openAIService.getImageDescription(imageUrl);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("test/openai/image")
+    public ResponseEntity<String> createImage(@RequestParam String gender, @RequestParam String action,
+        @RequestParam String animal) throws JsonProcessingException {
+        String result = openAIService.createImage(openAIService.buildPrompt(gender, action, animal));
+        return ResponseEntity.ok(result);
     }
 
 }
