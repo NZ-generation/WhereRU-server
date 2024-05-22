aae674dbb05fd72efc9a27961d95d34da54c2ffc
yerim216 <bing_01@naver.com>
2024-05-23 01:44:47 +0900

    Feat: FeignClient 추가

--- src/main/java/com/example/nzgeneration/NzGenerationApplication.java
+++ src/main/java/com/example/nzgeneration/NzGenerationApplication.java
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @OpenAPIDefinition(servers={
     @Server(url ="/", description = "Default Server URL")
 })
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableFeignClients
 public class NzGenerationApplication {
 
     public static void main(String[] args) {
