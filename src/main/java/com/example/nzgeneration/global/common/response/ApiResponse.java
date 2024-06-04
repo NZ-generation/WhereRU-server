15a2178b7b3f23e75fbec580477e272b43bd6dc6
yerim216 <bing_01@naver.com>
2024-06-04 18:21:49 +0900

    Docs: Swagger 설명 추가

--- src/main/java/com/example/nzgeneration/global/common/response/ApiResponse.java
+++ src/main/java/com/example/nzgeneration/global/common/response/ApiResponse.java
@@ -4,6 +4,8 @@ import com.example.nzgeneration.global.common.response.code.status.SuccessStatus
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -28,5 +30,18 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> onFailure(int code, String message, T data){
         return new ApiResponse<>(false, code, message, data);
     }
+
+    // result 필드를 JSON 형식으로 변환하는 메소드
+    public String getResultAsJson() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(this.result);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 
 }
